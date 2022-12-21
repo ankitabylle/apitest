cac0066e67204d6e01434e5521f4dffa5ba571a1
ankitaman98 <05ankit06@gmail.com>
2022-12-21 17:12:58 +0530

    test

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -25,7 +25,7 @@ namespace test1.Controllers
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                Summary = Summaries[2]
             })
             .ToArray();
         }
